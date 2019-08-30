5280462fa88e435f2e183379f11e332c5ca9e3e9
doanga2007 <wig_2006@hotmail.com>
2019-08-30 10:34:49 +0700

    Update.

---
+++ ArraySumInASPNET/Default.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ArraySumInASPNET
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string[] arr = new string[]
+            {
+            "200",
+            "500",
+            "900"
+            };
+
+            for (int k = 0; k < arr.Length; k++)
+            {
+                double num = Convert.ToDouble(arr[k]);
+                long num2 = Convert.ToInt64(num);
+
+                num2 += num2;
+
+                string num3 = Convert.ToString(num2);
+
+                sum1.Text = num3;
+            }
+        }
+    }
+}
